8981e4bb3a2cdd2433fdfffa4ee158c217ed95ae
Janisroz <janisrozkalejs@gmail.com>
2023-07-02 13:15:13 +0000

    Validate JS and clean up

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -17,29 +17,29 @@ let selection = [
         emoji: "✌️",
         beats: "paper"
     }
-]
+];
 
 // Define rounds and playerName for access at end of game 
-let rounds = ""
-let playerName = ""
+let rounds = "";
+let playerName = "";
 
 // Start with opening the Modal pop-up
 document.getElementById("choose-game").addEventListener("click", function modal() {
-    let backdrop = document.getElementById("start-game-background")
-    let modal = document.getElementById("modal-pop-up")
-    backdrop.style.display = 'block'
-    modal.style.display = 'block'
-    gameMode()
-})
+    let backdrop = document.getElementById("start-game-background");
+    let modal = document.getElementById("modal-pop-up");
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+    gameMode();
+});
 
 // Allow user to click outside of the modal to close it
 document.getElementById("start-game-background").addEventListener("click", function () {
-    let backdrop = document.getElementById("start-game-background")
-    let modal = document.getElementById("modal-pop-up")
-    backdrop.style.display = 'none'
-    modal.style.display = 'none'
+    let backdrop = document.getElementById("start-game-background");
+    let modal = document.getElementById("modal-pop-up");
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
 
-})
+});
 
 /**
  * Allows user to choose how long the game will be before starting game 
@@ -47,21 +47,21 @@ document.getElementById("start-game-background").addEventListener("click", funct
 function gameMode() {
     // listen to the users choices to decide how many rounds to go for
 
-    let roundSelect = document.getElementsByName("best-of")
-    for (i = 0; i < roundSelect.length; i++)
+    let roundSelect = document.getElementsByName("best-of");
+    for (let i = 0; i < roundSelect.length; i++) {
         roundSelect[i].addEventListener("click", function () {
-            rounds = this.getAttribute('value')
-        })
-
+            rounds = this.getAttribute('value');
+        });
+    }
 
     document.getElementById("start-game").addEventListener("click", function () {
-        playerName = document.getElementById("name").value
+        playerName = document.getElementById("name").value;
         if (playerName.trim().length > 0 && (rounds === "best-of-3" || rounds === "best-of-5" || rounds === "best-of-7")) {
-            startGame(rounds, playerName)
+            startGame(rounds, playerName);
         } else {
-            alert("Please enter your name & choose a actual number of rounds")
+            alert("Please enter your name & choose a actual number of rounds");
         }
-    })
+    });
 }
 
 
@@ -71,55 +71,40 @@ function gameMode() {
  * @param {*} rounds 
  */
 function startGame(rounds, playerName) {
-    // hide endGame modal if restarting
-    document.getElementById("end-game").style.display = "none"
-    document.getElementById("end-game-backdrop").style.display = "none"
-
-    // Reset player score 
-    let prevPscore = document.getElementById("p-score");
-    prevPscore.innerText = "0";
-
-
-    // Reset computer score 
-    let computerScore = document.getElementById("c-score");
-    computerScore.innerText = "0";
-
     // remove the backdrop modal and instruction screens
-    let backdrop = document.getElementById("start-game-background")
-    let modal = document.getElementById("modal-pop-up")
-    let startScreen = document.getElementById("instruction-section")
-    backdrop.style.display = 'none'
-    modal.style.display = 'none'
-    startScreen.style.display = 'none'
+    let backdrop = document.getElementById("start-game-background");
+    let modal = document.getElementById("modal-pop-up");
+    let startScreen = document.getElementById("instruction-section");
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+    startScreen.style.display = 'none';
 
     // Display the game screen
-    let game = document.getElementById("game-section")
-    game.style.display = 'block'
+    let game = document.getElementById("game-section");
+    game.style.display = 'block';
 
     // insert player name into player-name span
-    let playerNamePosition = document.getElementById("player-name")
-    playerNamePosition.innerText = playerName
+    let playerNamePosition = document.getElementById("player-name");
+    playerNamePosition.innerText = playerName;
 
     // insert player name into score span
-    let playerScore = document.getElementById("pScore")
-    playerScore.innerHTML = `${playerName}'s Score <span id="p-score">0</span>`
+    let playerScore = document.getElementById("pScore");
+    playerScore.innerHTML = `${playerName}'s Score <span id="p-score">0</span>`;
 
     // run game function
-    pvc()
+    pvc();
 
     // get the number of rounds 
-    let roundText = document.getElementById("nb-rounds")
+    let roundText = document.getElementById("nb-rounds");
     if (rounds === "best-of-3") {
-        roundText.innerText = "Best of 3"
+        roundText.innerText = "Best of 3";
     } else if (rounds === "best-of-5") {
-        roundText.innerText = "Best of 5"
+        roundText.innerText = "Best of 5";
     } else if (rounds === "best-of-7") {
-        roundText.innerText = "Best of 7"
+        roundText.innerText = "Best of 7";
     } else {
-        alert("No Rounds Selected defaulting to Best of 3")
+        alert("No Rounds Selected defaulting to Best of 3");
     }
-
-
 }
 
 
@@ -127,129 +112,88 @@ function startGame(rounds, playerName) {
  * Player vs Computer game mode 
  */
 function pvc() {
-       // Clear prev player choice emojis
-       let pEmojis = document.getElementById("pScore");
-       let child = pEmojis.nextElementSibling;
-   
-       while (child !== null && child.tagName === 'DIV') {
-           let currentDiv = child;
-           child = currentDiv.nextElementSibling;
-           currentDiv.remove();
-       }
-   
-       // Clear prev computer choice emojis
-       let cEmojis = document.getElementById("cScore");
-       child = cEmojis.nextElementSibling;
-   
-       while (child !== null && child.tagName === 'DIV') {
-           currentDiv = child;
-           child = currentDiv.nextElementSibling;
-           currentDiv.remove();
-       }
-   
-       
-
     // listen for player choice & get computer choice run checkwinner functions to decide winner
-    let selectionButton = document.getElementsByClassName("selection")
-    for (i = 0; i < selectionButton.length; i++) {
-        selectionButton[i].addEventListener("click", function () {
-            console.log("clicked")
-            let selectionChoice = this.getAttribute("data-selection")
-            let pChoice = selection.find(selection => selection.name === selectionChoice)
-            let cChoice = computerChoice()
-            checkWinnerPlayer2(cChoice, pChoice)
-            checkWinnerPlayer1(pChoice, cChoice)
-            endGame()
-        })
+    let selectionButton = document.getElementsByClassName("selection");
+    for (let i = 0; i < selectionButton.length; i++) {
+        selectionButton[i].addEventListener("click", function playerSelect() {
+            console.log("clicked");
+            let selectionChoice = this.getAttribute("data-selection");
+            let pChoice = selection.find(selection => selection.name === selectionChoice);
+            let cChoice = computerChoice();
+            checkWinnerPlayer2(cChoice, pChoice);
+            checkWinnerPlayer1(pChoice, cChoice);
+            endGame();
+        });
 
     }
-
-    for (i = 0; i < selectionButton.length; i++) {
-        selectionButton[i].removeEventListener("click", function () {
-            console.log("clicked")
-            let selectionChoice = this.getAttribute("data-selection")
-            let pChoice = selection.find(selection => selection.name === selectionChoice)
-            let cChoice = computerChoice()
-            checkWinnerPlayer2(cChoice, pChoice)
-            checkWinnerPlayer1(pChoice, cChoice)
-            endGame()
-        })
-        
 }
 
-}
 
 /**
  * ends the game allowing user to restart the game or back to main screen
  */
 function endGame() {
     // Get the results of Player & Computer and check who is winner and display end game modal 
-    let pScore = document.getElementById("p-score").innerText
-    let cScore = document.getElementById("c-score").innerText
+    let pScore = document.getElementById("p-score").innerText;
+    let cScore = document.getElementById("c-score").innerText;
 
     // Get the best of text to see what score should be testing for 
-    let nbRounds = document.getElementById("nb-rounds").innerText
+    let nbRounds = document.getElementById("nb-rounds").innerText;
 
-    let winner = ""
+    let winner = "";
     if (nbRounds === "Best of 3") {
         if (pScore === "2") {
             winner = "Player";
-            endGameModal(winner)
+            endGameModal(winner);
         } else if (cScore === "2") {
             winner = "Computer";
-            endGameModal(winner)
+            endGameModal(winner);
         }
     } else if (nbRounds === "Best of 5") {
         if (pScore === "3") {
             winner = "Player";
-            endGameModal(winner)
+            endGameModal(winner);
         } else if (cScore === "3") {
             winner = "Computer";
-            endGameModal(winner)
+            endGameModal(winner);
         }
     } else if (nbRounds === "Best of 7") {
         if (pScore === "4") {
             winner = "Player";
-            endGameModal(winner)
+            endGameModal(winner);
         } else if (cScore === "4") {
             winner = "Computer";
-            endGameModal(winner)
+            endGameModal(winner);
         }
     }
 }
 
 function endGameModal(winner) {
-
-
-    document.getElementById("end-game").style.display = "block"
-    document.getElementById("end-game-backdrop").style.display = "block"
+    document.getElementById("end-game").style.display = "block";
+    document.getElementById("end-game-backdrop").style.display = "block";
     // get winner name & Score and input into modal
-    let winnerName = document.getElementById("winner-name")
-    let playerName = document.getElementById("player-name")
-    let pName = playerName.innerText
+    let winnerName = document.getElementById("winner-name");
+    let playerName = document.getElementById("player-name");
     if (winner === "Player") {
         // insert winner name 
-        winnerName.innerText = playerName.innerText
-
+        winnerName.innerText = playerName.innerText;
         //insert winner score and the loser score
-        let winnerScore = document.getElementById("p-score").innerText
-        let loserScore = document.getElementById("c-score").innerText
-        let winnerSpan = document.getElementById("winner-score")
-        let loserSpan = document.getElementById("loser-score")
-        winnerSpan.innerText = winnerScore
-        loserSpan.innerText = loserScore
-
+        let winnerScore = document.getElementById("p-score").innerText;
+        let loserScore = document.getElementById("c-score").innerText;
+        let winnerSpan = document.getElementById("winner-score");
+        let loserSpan = document.getElementById("loser-score");
+        winnerSpan.innerText = winnerScore;
+        loserSpan.innerText = loserScore;
     } else if (winner === "Computer") {
         // insert winner name 
-        winnerName.innerText = "Computer"
-
+        winnerName.innerText = "Computer";
         //insert winner score and the loser score
-        let winnerScore = document.getElementById("c-score").innerText
-        let loserScore = document.getElementById("p-score").innerText
-        let winnerSpan = document.getElementById("winner-score")
-        let loserSpan = document.getElementById("loser-score")
-        winnerSpan.innerText = winnerScore
-        loserSpan.innerText = loserScore
+        let winnerScore = document.getElementById("c-score").innerText;
+        let loserScore = document.getElementById("p-score").innerText;
+        let winnerSpan = document.getElementById("winner-score");
+        let loserSpan = document.getElementById("loser-score");
+        winnerSpan.innerText = winnerScore;
+        loserSpan.innerText = loserScore;
     }
 
 
@@ -268,50 +212,25 @@ function endGameModal(winner) {
     child = cEmojis.nextElementSibling;
 
     while (child !== null && child.tagName === 'DIV') {
-        currentDiv = child;
+        let currentDiv = child;
         child = currentDiv.nextElementSibling;
         currentDiv.remove();
     }
 
-    // set up buttons to restart game or return to main screen
-    let playAgain = document.getElementById("play-again")
-    
-    
-
-    playAgain.addEventListener("click", function () {
-        
-        let selectionButton = document.getElementsByClassName("selection")
-        
-    startGame(rounds, pName)
-    })
-
-    let homeScreen = document.getElementById("restart")
+    // set up button to return to main screen
+    let homeScreen = document.getElementById("restart");
     homeScreen.addEventListener("click", function () {
-        // let startScreen = document.getElementById("instruction-section")
-        // document.getElementById("end-game").style.display = "none"
-        // document.getElementById("end-game-backdrop").style.display = "none"
-        // startScreen.style.display = 'block'
-        // let game = document.getElementById("game-section")
-        // game.style.display = 'none'
-        // // clear prev name
-        // document.getElementById("name").value = ""
-        // // clear radio buttons 
-        // let roundSelection = document.getElementsByName("best-of")
-        // for (i = 0; i < roundSelection.length; i++) {
-        //     roundSelection[i].checked = false;
-        // }
-        location.reload()
-
-    })
+        location.reload();
+    });
 }
 
 /**
  * Randomly generate computers choice
  */
 function computerChoice() {
-    let randomNb = Math.floor(Math.random() * 3)
-    let cChoice = selection[randomNb]
-    return cChoice
+    let randomNb = Math.floor(Math.random() * 3);
+    let cChoice = selection[randomNb];
+    return cChoice;
 }
 
 /**
@@ -322,33 +241,33 @@ function computerChoice() {
 function checkWinnerPlayer1(pChoice, cChoice) {
     if (cChoice.name === pChoice.beats) {
         // means player 1 won emoji and insert into location add 1 to score
-        let finalDiv = document.getElementById("pScore")
-        let scoreDiv = document.createElement("div")
-        scoreDiv.innerText = pChoice.emoji
-        scoreDiv.style.fontSize = '4rem'
-        scoreDiv.classList.add("player-score-display")
-        finalDiv.after(scoreDiv)
+        let finalDiv = document.getElementById("pScore");
+        let scoreDiv = document.createElement("div");
+        scoreDiv.innerText = pChoice.emoji;
+        scoreDiv.style.fontSize = '4rem';
+        scoreDiv.classList.add("player-score-display");
+        finalDiv.after(scoreDiv);
 
         // get p-score and add 1
-        let score = document.getElementById('p-score')
-        let scoreNb = score.innerHTML
-        console.log(scoreNb)
-        score.innerHTML = ++scoreNb
+        let score = document.getElementById('p-score');
+        let scoreNb = score.innerHTML;
+        console.log(scoreNb);
+        score.innerHTML = ++scoreNb;
     } else if (cChoice.name === pChoice.name) {
         // means its a draw just add emoji into location 
-        let finalDiv = document.getElementById("pScore")
-        let scoreDiv = document.createElement("div")
-        scoreDiv.innerText = pChoice.emoji
-        scoreDiv.classList.add("player-score-display")
-        finalDiv.after(scoreDiv)
+        let finalDiv = document.getElementById("pScore");
+        let scoreDiv = document.createElement("div");
+        scoreDiv.innerText = pChoice.emoji;
+        scoreDiv.classList.add("player-score-display");
+        finalDiv.after(scoreDiv);
 
     } else {
         // means player1 lost get emoji and insert into location 
-        let finalDiv = document.getElementById("pScore")
-        let scoreDiv = document.createElement("div")
-        scoreDiv.innerText = pChoice.emoji
-        scoreDiv.classList.add("player-score-display")
-        finalDiv.after(scoreDiv)
+        let finalDiv = document.getElementById("pScore");
+        let scoreDiv = document.createElement("div");
+        scoreDiv.innerText = pChoice.emoji;
+        scoreDiv.classList.add("player-score-display");
+        finalDiv.after(scoreDiv);
     }
 
 }
@@ -361,34 +280,33 @@ function checkWinnerPlayer1(pChoice, cChoice) {
 function checkWinnerPlayer2(cChoice, pChoice) {
     if (pChoice.name === cChoice.beats) {
         // means player 2 won emoji and insert into location add 1 to score
-        let finalDiv = document.getElementById("cScore")
-        let scoreDiv = document.createElement("div")
-        scoreDiv.innerText = cChoice.emoji
-        scoreDiv.style.fontSize = '4rem'
-        scoreDiv.classList.add("player-score-display")
-        finalDiv.after(scoreDiv)
+        let finalDiv = document.getElementById("cScore");
+        let scoreDiv = document.createElement("div");
+        scoreDiv.innerText = cChoice.emoji;
+        scoreDiv.style.fontSize = '4rem';
+        scoreDiv.classList.add("player-score-display");
+        finalDiv.after(scoreDiv);
 
         // get c-score and add 1
-        let score = document.getElementById('c-score')
-        let scoreNb = score.innerHTML
-        console.log(scoreNb)
-        score.innerHTML = ++scoreNb
+        let score = document.getElementById('c-score');
+        let scoreNb = score.innerHTML;
+        console.log(scoreNb);
+        score.innerHTML = ++scoreNb;
 
     } else if (cChoice.name === pChoice.name) {
         // means its a draw just add emoji into location 
-        let finalDiv = document.getElementById("cScore")
-        let scoreDiv = document.createElement("div")
-        scoreDiv.innerText = cChoice.emoji
-        scoreDiv.classList.add("player-score-display")
-        finalDiv.after(scoreDiv)
+        let finalDiv = document.getElementById("cScore");
+        let scoreDiv = document.createElement("div");
+        scoreDiv.innerText = cChoice.emoji;
+        scoreDiv.classList.add("player-score-display");
+        finalDiv.after(scoreDiv);
     } else {
         // means player2 lost get emoji and insert into location 
-        let finalDiv = document.getElementById("cScore")
-        let scoreDiv = document.createElement("div")
-        scoreDiv.innerText = cChoice.emoji
-        scoreDiv.classList.add("player-score-display")
-        finalDiv.after(scoreDiv)
+        let finalDiv = document.getElementById("cScore");
+        let scoreDiv = document.createElement("div");
+        scoreDiv.innerText = cChoice.emoji;
+        scoreDiv.classList.add("player-score-display");
+        finalDiv.after(scoreDiv);
     }
-
 }
 
